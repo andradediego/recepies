1bf0f61c9ffc26bfcee9535332145c6fa9fc3d5a
Diego <andrade.diego@outlook.com>
2018-05-14 22:49:30 -0300

    added a lot of featured modules

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,20 @@
+import { AuthRoutingModule } from './auth-routing.module';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
+
+@NgModule({
+  declarations: [
+    SigninComponent,
+    SignupComponent
+  ],
+  imports: [
+    FormsModule,
+    AuthRoutingModule
+  ]
+})
+export class AuthModule {
+
+}
--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -2,16 +2,15 @@ import {
   Directive,
   HostListener,
   HostBinding
-} from '@angular/core'
+} from '@angular/core';
 
 @Directive({
   selector: '[appDropdown]'
 })
-
 export class DropdownDirective {
   @HostBinding('class.open') isOpen = false;
 
-  @HostListener('click') toggleOpen(){
+  @HostListener('click') toggleOpen() {
     this.isOpen = !this.isOpen;
   }
 }
